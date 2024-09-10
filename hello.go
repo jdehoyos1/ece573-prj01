ec51851f5a12be120d1ddf1e19acbafc3569b861
Javier de Hoyos <jdehoyos1@hawk.iit.edu>
2024-09-10 17:41:09 +0000

    prj01-date/time&CWID

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
+	dt:=time.Now()
 	fmt.Printf("Hello world!\n")
+	fmt.Printf("It's ")
+	fmt.Printf(dt.Format("02-01-2006 15:04:05"))
+	fmt.Printf(" now and my CWID ending in 9715")
+
 }
